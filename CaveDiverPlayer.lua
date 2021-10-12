72c6425a3ffa182df60510f9d451ff02ffd65ca6
PNadiadhara <43886212+PNadiadhara@users.noreply.github.com>
2021-10-12 17:18:43 -0400

    initial commit

---
+++ CaveDiverPlayer.lua
@@ -0,0 +1,27 @@
+function make_player()
+	--in lua this table acts
+		-- similar to object
+	player={}
+	player.x=24 --xy plyr position
+	player.y=60
+	
+	player.dy=0 --fall speed
+
+	player.rise=1 --sprites
+	player.fall=2
+	player.dead=3
+	
+	player.speed=2 --fly speed 
+	player.score=0
+end
+
+function draw_player()
+	if (game_over) then
+	--ref spr in doc for more info
+		spr(player.dead,player.x,player.y)
+	elseif (player.dy<0) then
+		spr(player.rise,player.x,player.y)
+	else 
+		spr(player.fall,player.x,player.y)		
+	end
+end
