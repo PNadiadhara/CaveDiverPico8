de08a771fda736c15dd0c0f46e9b48abec137bfa
PNadiadhara <43886212+PNadiadhara@users.noreply.github.com>
2021-10-13 15:31:07 -0400

    cave auto generates

--- CaveDiverMain.lua
+++ CaveDiverMain.lua
@@ -1,14 +1,40 @@
-function _init()
-	game_over=false
-	make_player()
+function make_player()
+	--in lua this table acts
+		-- similar to object
+	player={}
+	player.x=24 --xy plyr position
+	player.y=60
+	
+	player.dy=0 --fall speed
+
+	player.rise=1 --sprites
+	player.fall=2
+	player.dead=3
+	
+	player.speed=2 --fly speed 
+	player.score=0
 end
 
-function _update()
-	move_player()
+function draw_player()
+	if (game_over) then
+	--ref spr in doc for more info
+		spr(player.dead,player.x,player.y)
+	elseif (player.dy<0) then
+		spr(player.rise,player.x,player.y)
+	else 
+		spr(player.fall,player.x,player.y)		
+	end
 end
 
-function _draw()
-	cls()
-	draw_player()
+function move_player()
+	gravity=0.2
+	player.dy+=gravity
+	
+	--jump
+	if(btn(2)) then
+		player.dy-=5
+		end
+	
+	--move to new pos
+	player.y+=player.dy
 end
-
